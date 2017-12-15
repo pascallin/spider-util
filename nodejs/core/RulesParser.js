e51fd08315f646fefafb4b5a2dbbea1168732208
pascal_lin <tantunlin@pengpengkeng.com>
2017-12-15 19:04:35 +0800

    update rules parser

--- nodejs/core/RulesParser.js
+++ nodejs/core/RulesParser.js
@@ -2,28 +2,24 @@ module.exports = {
     execute($, rule){
         let result;
         let elem = $(rule.rule);
+        // set type
+        if (!rule.type) rule.type = 'string'
+        // pre action
         if (rule.eq){
             elem = elem.eq(parseInt(rule.eq));
         }
-        if (rule.action === 'attr'){
-            result = elem.attr(rule.attr);
-        }
-        if (rule.action === 'css'){
-            result = elem.css(rule.css);
-        }
-        if (rule.action === 'text' && (!rule.type || rule.type === 'string')){
-            result = elem.text();
-        }
-        if (rule.action === 'text' && rule.type && rule.type === 'list'){
+        // action
+        if (rule.type === 'list'){
             result = [];
             elem.each(function(i, el){
-                let text = $(this).text();
-                result.push(text);
+                let temp = parseAction($(this), rule)
+                result.push(temp);
             });
         }
-        if (rule.action === 'html' && (!rule.type || rule.type === 'string')){
-            result = elem.html();
+        if (rule.type === 'string'){
+            result = parseAction(elem, rule)
         }
+        // after action
         if (_.has(rule, 'match') || _.has(rule, 'replace') || _.has(rule, 'exec')){
             if (typeof(result) === 'string'){
                 result = parseTextFilter(result, rule);
@@ -35,9 +31,20 @@ module.exports = {
         }
         return result;
     }
-} 
+}
 
-function parseTextFilter(text, rule){
+function parseAction (elem, rule) {
+    let result
+    switch (rule.action) {
+        case "text": result = elem.text(); break
+        case "css": result = elem.css(rule.css); break
+        case "attr": result = elem.attr(rule.attr); break
+        case "html": result = elem.html(); break
+    }
+    return result
+}
+
+function parseTextFilter (text, rule) {
     if (rule.match && text) text = text.match(eval(rule.match))[0];
     if (rule.replace && text) text = text.replace(eval(rule.replace),'');
     if (rule.exec && text){
--- nodejs/rules/easymoney.js
+++ nodejs/rules/easymoney.js
@@ -3,5 +3,11 @@ module.exports = {
         rule: '.nlist ul li',
         type: 'list',
         action: 'text'
+    },
+    nlist_link: {
+        rule: '.nlist ul li a',
+        type: 'list',
+        action: 'attr',
+        attr: 'href'
     }
 }
